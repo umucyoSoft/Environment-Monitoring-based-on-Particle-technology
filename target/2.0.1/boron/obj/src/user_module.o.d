d485e86264e944a09f1d005be0699840b48878ac
jean louis niragire <jeanbaty02@gmail.com>
2022-03-19 11:27:46 +0100

    Add files via upload

---
+++ target/2.0.1/boron/obj/src/user_module.o.d
@@ -0,0 +1,8 @@
+/Users/abdulhannanmustajab/Desktop/IoT/GLE_Chili_Dryer/GLE_Chili/target/2.0.1/boron//obj/./src/user_module.o: \
+ src/user_module.c \
+ ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
+ ../../../dynalib/inc/dynalib.h
+
+../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
+
+../../../dynalib/inc/dynalib.h:
